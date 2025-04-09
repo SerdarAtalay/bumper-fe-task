efe9ac48aaa6ac8a057159abe3643889d60a3ad4
SerdarAtalay <fserdaratalay@gmail.com>
2025-04-10 00:04:08 +0300

    fix: lil ui updates (most of them are stashed tho)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
-import Head from "next/head";
-import "../styles/globals.css";
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Exo&display=swap"
-          rel="stylesheet"
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Oswald:wght@700&display=swap" 
+          rel="stylesheet" 
         />
       </Head>
       <Component {...pageProps} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx}",
-      "./src/components/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['"Open Sans"', 'sans-serif'],
-          heading: ['"Futura"', '"Exo"', 'sans-serif'],
-          mono: ['var(--font-geist-mono)', 'monospace'],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bumper': {
+          'primary': '#FF733C',
+          'secondary': '#414B6E',
+          'accent': '#32BE50',
+          'contrast': '#1B1B1B',
         }
+      },
+      fontFamily: {
+        'oswald': ['Oswald', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+        'universal-sans': ['UniversalSans-850', 'sans-serif'],
       }
     },
-    plugins: []
-  };
+  },
+  plugins: [],
+};
